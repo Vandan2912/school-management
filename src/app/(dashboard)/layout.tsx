ca384005d7b14d7c15f20964cf2d1a54363adb7b
vandan <vandanpatel29122001@gmail.com>
2024-08-22 09:52:45 +0530

    layout change

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -10,8 +10,7 @@ export default function DashboardLayout({
 }>) {
   return (
     <div className="h-screen flex">
-      {/* LEFT */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+      {/* <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
         <Link
           href="/"
           className="flex items-center justify-center lg:justify-start gap-2"
@@ -21,11 +20,10 @@ export default function DashboardLayout({
         </Link>
         <Menu />
       </div>
-      {/* RIGHT */}
       <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
-        <Navbar />
-        {children}
-      </div>
+        <Navbar /> */}
+      {children}
+      {/* </div> */}
     </div>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
+import Image from "next/image";
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +20,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="h-screen flex">
+          {/* LEFT */}
+          <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+            <Link
+              href="/"
+              className="flex items-center justify-center lg:justify-start gap-2"
+            >
+              <Image src="/logo.png" alt="logo" width={32} height={32} />
+              <span className="hidden lg:block font-bold">SchooLama</span>
+            </Link>
+            <Menu />
+          </div>
+          {/* RIGHT */}
+          <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
+            <Navbar />
+            {children}
+          </div>
+        </div>
+
+        {/* {children} */}
+      </body>
     </html>
   );
 }
